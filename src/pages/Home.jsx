32cdd44f6ad5f47d652c8b6b5de946d265d98f53
Romtouf <romtouf@laposte.net>
2024-07-08 19:23:20 +0200

    Mise en place du projet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div className="home">
       <header className="header">
         <h1>Bienvenue chez Meta4invest</h1>
+        <p>TEST</p>
         <p>
           Votre expert en conseil et placement financier. Nous offrons une
           rentabilité élevée, une disponibilité des fonds sous 30 jours, et une
